824ff41f66a1f27fa8c2fc6a5168e8ab8e941bc0
ian <ian_lin@joy.com.tw>
2024-12-10 15:09:11 +0800

    update

--- backend/main.py
+++ backend/main.py
@@ -220,3 +220,10 @@ app.include_router(api_router, prefix="/api")
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))  # 使用 Railway 分配的 PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)  # 監聽所有網路介面
+
